d967f0189738d8b76ca29a04ec7bcaf23bbd3604
max <88110max123@gmail.com>
2023-08-25 18:29:34 +0800

    feat(BackGround)new wrapper RWD_width:650px

--- components/BackGround.jsx
+++ components/BackGround.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import style from './styles/BackGround.module.scss'
-import Image from 'next/image'
 
 const BackGround = () => {
   return (
-    <Image
-        className={style.Background}
-        width={1680}
-        height={1200}
-        src='/background.jpg'
-        alt='background'
-    />
+    <div className={style.Background}>
+      <div class={style.wrapper}>
+        <h2>Hi, I'm Max Liang</h2>
+        <h3>A Junior Front-end developer from Taiwan</h3>
+        <p>I embrace a passion for making a world-changing impact.</p>
+        <p>In pursuit of this goal,</p>
+        <p>I am honing my skills in areas such as Front-end development, UIUX, Product Management and more</p>
+      </div>
+      <div className={style.otherlink}>
+        {/* <a>
+          <img className={style.Linkedin} src="/linkedin.png" alt="Linkedin" />
+        </a>     */}
+      </div>
+    </div>
   )
 }
-
 export default BackGround
